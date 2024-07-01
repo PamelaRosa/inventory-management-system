2bc1e9c5e3ffdaf9b4d1eee32308bd70c0bb72ab
PamelaRosa <pa.rosa4@gmail.com>
2024-07-01 00:32:38 -0300

    Ajuste rotas

--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -5,14 +5,14 @@ const categoryController = new CategoryController();
 
 const router = Router();
 
-router.get('/users', (req, res) => categoryController.getAll(req, res));
-router.get('/users/:id', (req, res) => categoryController.getOne(req, res));
+router.get('/categories', (req, res) => categoryController.getAll(req, res));
+router.get('/categories/:id', (req, res) => categoryController.getOne(req, res));
 
-router.post('/users', (req, res) => categoryController.post(req, res));
+router.post('/categories', (req, res) => categoryController.post(req, res));
 
-router.put('/users/:id', (req, res) => categoryController.update(req, res));
+router.put('/categories/:id', (req, res) => categoryController.update(req, res));
 
-router.delete('/users/:id', (req, res) => categoryController.delete(req, res));
+router.delete('/categories/:id', (req, res) => categoryController.delete(req, res));
 
 
 module.exports = router;
