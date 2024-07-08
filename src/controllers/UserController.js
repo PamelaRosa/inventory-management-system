df7d4289ff750afd1558035c44c3cc13992a3abd
PamelaRosa <pa.rosa4@gmail.com>
2024-07-08 15:51:57 -0300

    Implementa metodos de registro e criação de conta

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,6 +8,72 @@ class UserController extends Controller {
   constructor() {
     super(userServices);
   }
+
+  async register(req, res) {
+    try {
+      const data = req.body;
+      if(!data.email || !data.password || !data.name) {
+        return res.status(422).json({
+          message: 'Dados inválidos.'
+        });
+      }
+      const existingUser = await this.entityService.getUserByEmail(data.email);
+      if (existingUser) {
+        return res.status(400).json({
+          message: 'Este e-mail já está cadastrado.'
+        });
+      }
+
+      const user = await this.entityService.register(data);
+      return res.status(200).json({
+        user
+      });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const data = req.body;
+      const { id } = req.params;
+      if(!id || !data.email || !data.password || !data.name) {
+        return res.status(422).json({
+          message: 'Dados inválidos.'
+        });
+      }
+      const existingUser = await this.entityService.getUserByEmail(data.email);
+      if (existingUser && Number(existingUser.id) !== Number(id)) {
+        return res.status(400).json({
+          message: 'Este e-mail já está cadastrado.'
+        });
+      }
+
+      const user = await this.entityService.update(data, id);
+      return res.status(200).json({
+        user
+      });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await this.entityService.login(email, password);
+
+      if (!user) {
+        return res.status(404).json({ message: 'Email ou senha inválidos.' });
+      }
+
+      return res.status(200).json({
+        user
+      });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = UserController;
--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,8 +1,61 @@
 const Services = require('./Services');
+const bcrypt = require('bcrypt');
+const dataSource = require('../database/models');
 
 class UserServices extends Services {
   constructor() {
     super('User');
+  }
+
+  async register(user) {
+    const { name, email, password } = user;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const registeredUser = await dataSource[this.model].create({
+      name,
+      email,
+      password: hashedPassword
+    });
+    return {
+      id: registeredUser.id,
+      name: registeredUser.name,
+      email: registeredUser.email
+    };
+  }
+
+  async update(user, id) {
+    const { name, email, password } = user;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await dataSource[this.model].update({
+      name,
+      email,
+      password: hashedPassword
+    }, {
+      where: { id }
+    });
+    return {
+      id,
+      name,
+      email
+    };
+  }
+
+  async login(email, password) {
+    const userByEmail = await this.getUserByEmail(email);
+    if (userByEmail) {
+      const passwordMatches = await bcrypt.compare(password, userByEmail.password);
+      if (passwordMatches) {
+        return {
+          id: userByEmail.id,
+          name: userByEmail.name,
+          email: userByEmail.email
+        };
+      }
+    }
+    return null;
+  }
+
+  async getUserByEmail(email) {
+    return await dataSource[this.model].findOne({ where: { email } });
   }
 }
 
